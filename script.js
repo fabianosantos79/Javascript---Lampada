043277ec8f001d16a86628256a57a5e345c3f8eb
Fabiano <fabiano.ssantos79@gmail.com>
2022-02-10 21:23:32 -0300

    Commit do sistema

---
+++ script.js
@@ -0,0 +1,24 @@
+function ligada(){
+    let imagem = document.querySelector('.lampada');
+    imagem.setAttribute('src', 'imagens/ligada.jpg');
+}
+
+function desligada(){
+    let imagem = document.querySelector('.lampada');
+    imagem.setAttribute('src', 'imagens/desligada.jpg');
+}
+
+function quebrada(){
+    let imagem = document.querySelector('.lampada');
+    imagem.setAttribute('src', 'imagens/quebrada.jpg');
+}
+
+function entrou(){
+    let imagem = document.querySelector('.lampada');
+    imagem.setAttribute('src', 'imagens/ligada.jpg');
+}
+
+function saiu(){
+    let imagem = document.querySelector('.lampada');
+    imagem.setAttribute('src', 'imagens/desligada.jpg');
+}
